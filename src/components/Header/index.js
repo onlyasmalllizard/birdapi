99530e463ae4834b6c917d5d16d7e3929e47651f
Lizard Morrow <lizardmorrow@gmail.com>
2021-09-12 13:45:54 +0100

    Style Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
     <header>
       <h1>Build-a-Bird</h1>
       <nav>
-        <NavLink to="/">New Bird</NavLink>
-        <NavLink to="/favourites">Favourites</NavLink>
+        <NavLink to="/" exact activeClassName="active-link">
+          New Bird
+        </NavLink>
+        <NavLink to="/favourites" activeClassName="active-link">
+          Favourites
+        </NavLink>
       </nav>
     </header>
   );
