efcf2cad7e6959da9b6e5747fd6d2f4a98435d16
Lizard Morrow <lizardmorrow@gmail.com>
2021-09-12 14:02:04 +0100

    Style pending screen

--- src/components/Pending/index.js
+++ src/components/Pending/index.js
@@ -1,10 +1,12 @@
+import './pending.css';
+
 function Pending() {
   return (
-    <section>
-      <h2>Your bird is being built!</h2>
+    <section className="pending">
+      <h2>Your bird is being built...</h2>
       <img
         src="https://cdn.pixabay.com/photo/2013/07/13/13/46/tux-161507_960_720.png"
-        alt="Penguin with tools and hat"
+        alt="A penguin wearing a red baseball cap is holding a hammer with one hand and a toolbox in the other. Two nails are in its beak."
       />
     </section>
   );
